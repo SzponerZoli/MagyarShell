74510529c78a8750309cf1d74a54274b49dcbea3
SzponerZoli <119530750+SzponerZoli@users.noreply.github.com>
2024-07-18 07:49:57 +0200

    Update EdgeUninstall.bat

--- scripts/EdgeUninstall.bat
+++ scripts/EdgeUninstall.bat
@@ -5,7 +5,7 @@ sp 'HKCU:\Volatile Environment' 'Edge_Removal' @'
 $also_remove_webview = 1
 
 write-host "`nAz Edge el lesz tavolitva."
-write-host "`nFIGYELEM! A szkript lefuttatasa utan a rendszer ujratelepíteseig az Edge-t nem lehet ujratelepiteni!"
+write-host "`nFIGYELEM! A szkript lefuttatasa utan a rendszer ujratelepiteseig az Edge-t nem lehet ujratelepiteni!"
 $uconfirmation = Read-Host "`nFolytatja? Y = igen, N = nem"
 if ($uconfirmation -ne "Y") { exit }
 $host.ui.RawUI.WindowTitle = 'XtremeShell Edge Eltávollító szkript'
