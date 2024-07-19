44bd6352862f2f290e3a4a353d4ddec6e9376021
SzponerZoli <119530750+SzponerZoli@users.noreply.github.com>
2024-07-19 13:50:01 +0200

    Update download.bat

--- scripts/download.bat
+++ scripts/download.bat
@@ -2,7 +2,9 @@
 net session >nul 2>&1
 if %errorLevel% == 0 (
     echo Running with admin privileges.
-    curl -o XtremeShell.exe https://raw.githubusercontent.com/SzponerZoli/MagyarShell/main/XtremeShell.exe
+    curl -o "C:\Program Files\Szponer\MagyarShell\XtremeShell.exe" https://raw.githubusercontent.com/SzponerZoli/MagyarShell/main/XtremeShell.exe
+    echo Installer finished
+    pause
 ) else (
     echo Requesting admin privileges...
     powershell start-process download.bat -verb runas
