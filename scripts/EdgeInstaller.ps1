5e4c82b07aed9df02eee7666561cd544d7207764
SzponerZoli <119530750+SzponerZoli@users.noreply.github.com>
2024-07-31 21:52:05 +0200

    Update EdgeInstaller.ps1

--- scripts/EdgeInstaller.ps1
+++ scripts/EdgeInstaller.ps1
@@ -7,15 +7,15 @@ Write-Host "Visszaallitas megkezdese..."
 $policies = 'HKLM\SOFTWARE\Policies\Microsoft\EdgeUpdate'
 $software = 'HKLM\SOFTWARE\Microsoft\EdgeUpdate'
 reg delete $policies /f
-#reg delete $software /f
-#$remove_keys = @("Microsoft Edge", "Microsoft Edge Update", "Microsoft EdgeWebView", "Win32WebViewHost")
-#foreach ($key in $remove_keys) {
- #   foreach ($root in "HKCU", "HKLM") {
-  #      reg delete "$root\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$key" /f
-   # }
-#}
-#$IFEO = 'HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options'
-#reg delete "$IFEO\ie_to_edge_stub.exe" /f
+reg delete $software /f
+$remove_keys = @("Microsoft Edge", "Microsoft Edge Update", "Microsoft EdgeWebView", "Win32WebViewHost")
+foreach ($key in $remove_keys) {
+    foreach ($root in "HKCU", "HKLM") {
+        reg delete "$root\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$key" /f
+    }
+}
+
+$IFEO = 'HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options'
 reg delete "$IFEO\msedge.exe" /f
 Write-Host "Visszaallitas befejezve."
 Read-Host "Nyomja meg az Entert a kilepeshez..."
